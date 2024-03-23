577b323f3f5d2c3ac415c20b2becaae47693730f
rizkydimedan <rizkyanakmedan77@gmail.com>
2024-03-23 23:57:03 +0700

    Order.html

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -1,61 +1,25 @@
+// Order.html for Accordion Payment
 
+    const accordionBtns = document.querySelectorAll(".accordion-button-1");
 
-/*
-* accordionEl: HTML element (required)
-* accordionItems: array of accordion item objects (required)
-* options (optional)
-* instanceOptions (optional)
-*/
+    for (let index = 0; index < accordionBtns.length; index++) {
+        const btnAcc = accordionBtns[index];
+        const accContent = btnAcc.nextElementSibling.querySelector('.accordion-content-1');
 
-const accordionElement = document.getElementById('accordion-flush');
-
-// create an array of objects with the id, trigger element (eg. button), and the content element
-const accordionItems = [
-    {
-        id: 'accordion-flush-heading-1',
-        triggerEl: document.querySelector('#accordion-flush-heading-1'),
-        targetEl: document.querySelector('#accordion-flush-body-1'),
-        active: false
-    },
-    {
-        id: 'accordion-example-heading-2',
-        triggerEl: document.querySelector('#accordion-example-heading-2'),
-        targetEl: document.querySelector('#accordion-example-body-2'),
-        active: false
-    },
-    {
-        id: 'accordion-example-heading-3',
-        triggerEl: document.querySelector('#accordion-example-heading-3'),
-        targetEl: document.querySelector('#accordion-example-body-3'),
-        active: false
+        btnAcc.addEventListener('click', () => {
+            btnAcc.classList.toggle("active");
+            if (accContent && accContent.classList.contains("hidden")) {
+                accContent.classList.remove("hidden");
+            } else if (accContent) {
+                accContent.classList.add("hidden");
+            }
+        });
     }
-];
-
-// options with default values
-const options = {
-    alwaysOpen: true,
-    activeClasses: 'bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-white',
-    inactiveClasses: 'text-gray-500 dark:text-gray-400',
-
-};
-
-// instance options object
-const instanceOptions = {
-    id: 'accordion-example',
-    override: true
-};
 
 
-const accordion = new Accordion(accordionElement, accordionItems, options, instanceOptions);
 
-// open accordion item based on id
-accordion.close('accordion-flush-heading-1');
 
-// close accordion item based on id
-accordion.close('accordion-example-heading-2');
 
-// toggle visibility of item based on id
-accordion.close('accordion-example-heading-3');
 
 
 
