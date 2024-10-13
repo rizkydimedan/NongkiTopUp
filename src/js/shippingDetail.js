13b6edd4f937348886cf679d03ad0aaeda3db292
rizkydimedan <rizkyanakmedan77@gmail.com>
2024-10-13 20:03:29 +0700

    "Mark Game subproject as dirty"

--- src/js/shippingDetail.js
+++ src/js/shippingDetail.js
@@ -2,42 +2,44 @@ let data = {
   "id-user": '',
   "server-user": '',
   "phone-number": '',
-  payment: '',
+    // payment: '',
 };
 
-const inputs = document.querySelectorAll("#shipping-detail input[data-input]");
+// const inputs = document.querySelectorAll("#shipping-detail input[data-input]");
 
-for (let index = 0; index < inputs.length; index++) {
-  const input = inputs[index];
+// for (let index = 0; index < inputs.length; index++) {
+//   const input = inputs[index];
 
-  input.addEventListener("change", function (event) {
-    data[event.target.id] = event.target.value;
+//   input.addEventListener("change", function (event) {
+//     data[event.target.id] = event.target.value;
 
-    check();
-  });
-  // console.log(input);
-};
+//     check();
+//   });
+//   // console.log(input);
+// };
 
-const buttons = document.querySelectorAll('#shipping-detail button[data-name]');
+// const buttons = document.querySelectorAll('#shipping-detail button[data-name]');
 
-buttons.forEach(element => {
-  element.addEventListener('click', function () {
-    const value = this.attributes['data-value'].value;
-    const name = this.attributes['data-name'].value;
+// buttons.forEach(element => {
+//   element.addEventListener('click', function () {
+//     const value = this.attributes['data-value'].value;
+//     const name = this.attributes['data-name'].value;
 
-    data[name] = value;
-    check();
-  });
-  // console.log(element);
-});
+//     data[name] = value;
+//     check();
+//   });
+//   // console.log(element);
+// });
 
-function check() {
-  console.log(data);
-  const find = Object.values(data).filter((item) => item === "");
+// function check() {
+//   console.log(data);
+//   const find = Object.values(data).filter((item) => item === "");
 
-  if (find.length == 0) {
-    document.querySelector(
-      "#shipping-detail button[type='submit']"
-    ).disabled = false;
-  }
-};
+//   if (find.length == 0) {
+//     document.querySelector(
+//       "#shipping-detail button[type='submit']"
+//     ).disabled = false;
+//   }
+// };
+
+
